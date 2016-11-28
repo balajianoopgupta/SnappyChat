04ab2e3464a0e21580a4645dec30e893c788ceee
balajianoopgupta <balajianoopgupta.ba@gmail.com>
2016-11-27 21:15:15 -0800

    Removed unwanted code

--- app/src/main/java/com/cmpe277/snappychat/TimelineActivity.java
+++ app/src/main/java/com/cmpe277/snappychat/TimelineActivity.java
@@ -1,10 +1,10 @@
 package com.cmpe277.snappychat;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -18,7 +18,6 @@ import com.google.firebase.auth.FirebaseAuth;
 public class TimelineActivity extends AppCompatActivity {
     private static final String TAG = "Timeline Acitivity";
     Button logOut;
-    GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,10 +29,6 @@ public class TimelineActivity extends AppCompatActivity {
         logOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-//                FirebaseAuth.getInstance().signOut();
-//                LoginActivity.signOut();
-//                finish();
                 logOut();
             }
         });
